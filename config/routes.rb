67c922b08c5e2ec8b5922e0f7e9d90ac53328515
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-10-21 12:21:12 +0000

    [Update] comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   root to: 'homes#top'
   
-  get '/home/about' => 'homes#about'
-  
-  devise_for :users
+  get '/home/about' => 'homes#about', as: 'about'
   
   resources :users,only: [:show,:index,:edit,:update]
   
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
