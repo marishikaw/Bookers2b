ba5ca0130bacc1a388d0d88a11dc9d63a67a4cc2
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-10-28 11:23:01 +0000

    [Update] 10282020

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,17 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    # redirect_to book_path(book)
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 end
