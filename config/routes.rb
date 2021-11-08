6515ea712df6c8b01996c4158417af06f3ca2346
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-11-08 11:14:42 +0000

    [Update] task9_1108

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   
   get '/home/about' => 'homes#about', as: 'about'
-  get '/search', to: 'searches#search'
   
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    get '/search', to: 'searches#search'
   end
   
   resources :books do
